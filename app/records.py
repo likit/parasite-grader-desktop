ff5ef26b22fbe8c8d2b76d2501ee9fa758ae4933
Likit Preeyanon <likit.pre@mahidol.edu>
2023-03-04 10:28:12 +0700

    created a quick select window for a faster input

--- app/records.py
+++ app/records.py
@@ -3,6 +3,65 @@ import PySimpleGUI as sg
 from organisms import organisms, stages
 
 
+def create_batch_insert_window():
+    organism_list = []
+    organism_widgets = []
+    rows = []
+    for n, org in enumerate(organisms, start=0):
+        if n % 3 == 0:
+            rows.append(
+                sg.Checkbox(org, key=org,
+                            default=False,
+                            size=(18, 1),
+                            enable_events=True)
+            )
+            organism_widgets.append(rows)
+            rows = []
+        else:
+            rows.append(
+                sg.Checkbox(org,
+                            key=org,
+                            default=False,
+                            size=(18, 1),
+                            enable_events=True)
+            )
+    if rows:
+        organism_widgets.append(rows)
+
+    layout = [
+        organism_widgets,
+        [sg.Text('Total: '), sg.Text('0', key='-TOTAL-')],
+        [sg.Cancel(), sg.Ok()]
+    ]
+
+    window = sg.Window('Record Dialog',
+                       layout=layout,
+                       modal=True,
+                       resizable=True,
+                       finalize=True,
+                       use_default_focus=True,
+                       use_ttk_buttons=True
+                       )
+
+    while True:
+        event, values = window.read()
+        if event in [sg.WINDOW_CLOSED, 'Cancel']:
+            break
+        elif event == 'Ok':
+            for key, value in values.items():
+                if value:
+                    organism_list.append(key)
+            break
+        else:
+            total = len([(key, value) for key, value in values.items() if value])
+            window.find_element('-TOTAL-').update(str(total))
+            window.refresh()
+
+    window.close()
+
+    return organism_list
+
+
 def create_record_window(student_record, items):
     # check if the record exists
     record = [item for item in items if item[0] == student_record[1]]
@@ -14,27 +73,28 @@ def create_record_window(student_record, items):
             combos.append(
                 [
                     sg.Text(f'{i}:'),
-                    sg.Combo(organisms, key=f'-ORG{i}-', readonly=True),
-                    sg.Combo(stages, key=f'-STG{i}-', readonly=True),
+                    sg.Combo(organisms, key=f'-ORG{i}-', readonly=True, size=(16, 1)),
+                    sg.Combo(stages, key=f'-STG{i}-', readonly=True, size=(16, 1)),
                     sg.Checkbox('Rare', key=f'-RARE{i}-', default=False),
-                    sg.Combo(organisms, key=f'-KEYORG{i}-', readonly=True),
-                    sg.Combo(stages, key=f'-KEYSTG{i}-', readonly=True),
+                    sg.Combo(organisms, key=f'-KEYORG{i}-', readonly=True, size=(16, 1)),
+                    sg.Combo(stages, key=f'-KEYSTG{i}-', readonly=True, size=(16, 1)),
                 ],
             )
         else:
             combos.append(
                 [
                     sg.Text(f'{i}:'),
-                    sg.Combo(organisms, key=f'-ORG{i}-', readonly=True, default_value=item[2]),
-                    sg.Combo(stages, key=f'-STG{i}-', readonly=True, default_value=item[3]),
+                    sg.Combo(organisms, key=f'-ORG{i}-', readonly=True, default_value=item[2], size=(16, 1)),
+                    sg.Combo(stages, key=f'-STG{i}-', readonly=True, default_value=item[3], size=(16, 1)),
                     sg.Checkbox('Rare', key=f'-RARE{i}-', default=item[6]),
-                    sg.Combo(organisms, key=f'-KEYORG{i}-', readonly=True, default_value=item[4]),
-                    sg.Combo(stages, key=f'-KEYSTG{i}-', readonly=True, default_value=item[5]),
+                    sg.Combo(organisms, key=f'-KEYORG{i}-', readonly=True, default_value=item[4], size=(16, 1)),
+                    sg.Combo(stages, key=f'-KEYSTG{i}-', readonly=True, default_value=item[5], size=(16, 1)),
                 ],
             )
     layout = [
         [sg.Text('ID'), sg.Text(student_record[1])],
         [sg.Text('Name'), sg.Text(student_record[2])],
+        [sg.Button('Quick Select', key='-BATCH-')],
         [
             sg.Text('Organism', justification='center', expand_x=True),
             sg.Text('Stage', justification='center', expand_x=True),
@@ -42,15 +102,20 @@ def create_record_window(student_record, items):
             sg.Text('Correct Stage', justification='center', expand_x=True)
         ],
         *combos,
-        [sg.Button('Save')]
+        [sg.Cancel('Close', button_color='red'), sg.Button('Save')]
     ]
 
     window = sg.Window('Record Dialog', layout=layout)
 
     while True:
         event, values = window.read()
-        if event in [sg.WINDOW_CLOSED, 'Exit']:
+        if event in [sg.WINDOW_CLOSED, 'Exit', 'Close']:
             break
+        elif event == '-BATCH-':
+            organism_list = create_batch_insert_window()
+            for n, org in enumerate(organism_list):
+                window.find_element(f'-ORG{n}-').update(org)
+            window.refresh()
         elif event == 'Save':
             record = []
             for i in range(0, 10):
